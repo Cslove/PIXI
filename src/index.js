d8babb6c82f038a0cb2730c962c363e1de504bb3
cslove <969118892@qq.com>
2018-06-12 23:19:10 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import * as PIXI from 'pixi.js';
+import './index.css';
+import all from './img/practice.json'
+
+let Application = PIXI.Application,
+	Sprite = PIXI.Sprite,
+	Loader = PIXI.loader,
+	resources = PIXI.loader.resources;
+let app = new Application();
+	app.renderer.view.style.position = "absolute";
+	app.renderer.view.style.display = "block";
+	app.renderer.autoResize = true;
+	app.renderer.resize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(app.view);
+
+Loader.add('./img/practice.json').load(setup);
+
+console.log(PIXI.loader.resources)
+function setup(){
+	let id = PIXI.loader.resources["./img/practice.json"].textures;
+	
+	let sprite = new Sprite(id['down_bird0.png']);
+	sprite.x = 300;
+	sprite.y=500;
+	sprite.width = 200;
+	sprite.height = 200;
+	app.stage.addChild(sprite)
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	plugins: [
+	    new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			title: 'PIXI',
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NamedModulesPlugin()
+	],
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{test: /\.css$/,use: ['style-loader','css-loader']}
+		]
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './src',
+		hot: true
+	}
+	
+}
