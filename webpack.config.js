38cc60bc9b0f6f3e56a934c266aac89126c393eb
cslove <969118892@qq.com>
2018-06-13 17:57:14 +0800

    add docs

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 	mode: 'development',
 	entry: './src/index.js',
 	plugins: [
-	    new CleanWebpackPlugin(['dist']),
+	    new CleanWebpackPlugin(['docs']),
 		new HtmlWebpackPlugin({
 			title: 'PIXI',
 		}),
@@ -17,7 +17,7 @@ module.exports = {
 	],
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'docs')
 	},
 	module: {
 		rules: [
